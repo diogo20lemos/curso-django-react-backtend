1d0111af9237f04b7f4bf27eef97f1d2ecb39b3d
Cicero Diogo Lemos <diogolemos@EDiagnose.local>
2021-07-06 13:08:29 -0300

    First commit prepareing the project

---
+++ backend/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework import routers
+from .views import *
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'groups', GroupViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+]
